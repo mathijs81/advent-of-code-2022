a7d06ed66e1f5d8a59a704405e19b68c6c351cc8
Mathijs Vogelzang <mathijs@apptornado.com>
2022-12-01 07:26:17 +0100

    AOC day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,40 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        var max = 0
+        for (line in input) {
+            if (line.trim().isEmpty()) {
+                max = max.coerceAtLeast(sum)
+                sum = 0
+            } else {
+                sum += line.toInt()
+            }
+        }
+        max = max.coerceAtLeast(sum)
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list = mutableListOf<Int>()
+        var sum = 0
+        for (line in input) {
+            if (line.trim().isEmpty()) {
+                list.add(sum)
+                sum = 0
+            } else {
+                sum += line.toInt()
+            }
+        }
+        list.add(sum)
+
+        list.sortWith(Comparator.naturalOrder<Int>().reversed())
+        return list.subList(0, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
