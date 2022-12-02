c75c71df47765c2e1e44d1f2e8c894834ec1a69f
Mathijs Vogelzang <mathijs@apptornado.com>
2022-12-02 06:56:45 +0100

    Add readAsString method

--- src/Day01.kt
+++ src/Day01.kt
@@ -32,11 +32,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readAsLines("Day01_test")
     check(part1(testInput) == 24000)
     check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readAsLines("Day01")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,12 +5,14 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
-    .readLines()
+fun readAsLines(name: String) = File("src", "$name.txt")
+        .readLines()
+
+fun readAsString(name: String) = File("src", "$name.txt").readText()
 
 /**
  * Converts string to md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+        .toString(16)
+        .padStart(32, '0')
